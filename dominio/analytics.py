65d3a3c3f76f5dbd0e43a080c1bc645f55a008db
AndresRodriguez-coder <rodriguezavilaandresdavid@gmail.com>
2025-09-04 21:28:26 -0500

    Ultimo commit :(

--- dominio/analytics.py
+++ dominio/analytics.py
@@ -5,16 +5,15 @@ from .models import HarvestBatch, Plot
 
 
 modelo1 = HarvestBatch.objects.all()
-data1 = list(modelo1.values())
-df_Harvest = pd.DataFrame(data1)
-
-
 modelo2 = Plot.objects.all()
+
+data1 = list(modelo1.values())
 data2 = list(modelo2.values())
+
+
+df_Harvest = pd.DataFrame(data1)
 df_Plot = pd.DataFrame(data2)
 
+df_filtrado= pd.merge(df_Harvest, df_Plot, left_on="Plot_id_id", right_on="id", how="left")
 
-df_results = pd.DataFrame()
-
-df_results["kg_x_m2"] = df_Harvest["quantity_kg"]/ df_Plot["area"]
-
+df_filtrado["kg_x_m2"] = df_filtrado["quantity_kg"] / df_filtrado["area"]
