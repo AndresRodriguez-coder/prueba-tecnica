141ee58e55ea27d5cf1e0d727f760d2fb7509e1e
AndresRodriguez-coder <rodriguezavilaandresdavid@gmail.com>
2025-09-04 21:14:36 -0500

    Primer commit

--- dominio/analytics.py
+++ dominio/analytics.py
@@ -6,21 +6,15 @@ from .models import HarvestBatch, Plot
 
 modelo1 = HarvestBatch.objects.all()
 data1 = list(modelo1.values())
-df1 = pd.DataFrame(data1)
+df_Harvest = pd.DataFrame(data1)
 
-print(df1)
 
 modelo2 = Plot.objects.all()
 data2 = list(modelo2.values())
-df2 = pd.DataFrame(data2)
+df_Plot = pd.DataFrame(data2)
 
-print(df2)
 
-print(df1)
 df_results = pd.DataFrame()
 
-df_results["kg_x_m2"] = df1["quantity_kg"]/ df2["area"]
+df_results["kg_x_m2"] = df_Harvest["quantity_kg"]/ df_Plot["area"]
 
-
-
-print (df_results)
